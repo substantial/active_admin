7af1d2be1a063a70db725963a0156b110495fbb5
Shawn Dempsey <sndempsey@gmail.com>
2013-09-24 11:00:42 -0700

    add name attribute to select dropdown

--- lib/active_admin/views/components/scopes_dropdown.rb
+++ lib/active_admin/views/components/scopes_dropdown.rb
@@ -22,6 +22,7 @@ module ActiveAdmin
       end
 
       def build(scopes, options = {})
+        super(:name => 'scope')
         scopes.each do |scope|
           build_scope(scope, options) if call_method_or_proc_on(self, scope.display_if_block)
         end
